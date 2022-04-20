15bc2c2880714f4808a3368eed82334c574f5b8c
Patricio <patriciouuu@gmail.com>
2022-04-19 21:12:42 -0300

    Primer commit

---
+++ APIPortfolioArgProg/src/main/java/com/portfolioAPI/APIPortfolioArgProg/ApiPortfolioArgProgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioAPI.APIPortfolioArgProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPortfolioArgProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPortfolioArgProgApplication.class, args);
+	}
+
+}
---
+++ APIPortfolioArgProg/src/test/java/com/portfolioAPI/APIPortfolioArgProg/ApiPortfolioArgProgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioAPI.APIPortfolioArgProg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPortfolioArgProgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
