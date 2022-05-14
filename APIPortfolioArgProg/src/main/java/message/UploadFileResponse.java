6daa41e5e89dccf75451807d47b8cdfd1a2ef5b3
Patricio <patriciouuu@gmail.com>
2022-05-14 18:33:11 -0300

    Imagen perfil ok

---
+++ APIPortfolioArgProg/src/main/java/message/UploadFileResponse.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package message;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author patri
+ */
+@Getter @Setter
+public class UploadFileResponse {
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+    public UploadFileResponse(String fileName, String fileDownloadUri, String fileType, long size) {
+        this.fileName = fileName;
+        this.fileDownloadUri = fileDownloadUri;
+        this.fileType = fileType;
+        this.size = size;
+    }
+
+}
