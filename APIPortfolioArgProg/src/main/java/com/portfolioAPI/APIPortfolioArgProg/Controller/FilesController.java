ff3dee6ccd343cd94f919dfc6e963603bc70b5f2
Patricio <patriciouuu@gmail.com>
2022-05-15 10:15:29 -0300

    Back listo con delete img

--- APIPortfolioArgProg/src/main/java/com/portfolioAPI/APIPortfolioArgProg/Controller/FilesController.java
+++ APIPortfolioArgProg/src/main/java/com/portfolioAPI/APIPortfolioArgProg/Controller/FilesController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,11 @@ public class FilesController {
         Resource file = storageService.load(filename);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+    
+    @DeleteMapping("/files/delete")
+    public void deleteImages(){
+        storageService.deleteAll();
     }
 
 }
